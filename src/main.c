2457cb79407d6970b5dff045817250aae8926151
unknown <terrencezcao@gmail.com>
2025-06-27 20:30:53 -0700

    Hello World Script

--- src/main.c
+++ src/main.c
@@ -1,10 +1,8 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "driver/uart.h"
 
 void app_main() {
-  while (1) {
-    printf("Hello, World!\n");  // ESP-IDF uses printf() instead of Serial
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
+    while (1) {
+        printf("Hello, World!\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
