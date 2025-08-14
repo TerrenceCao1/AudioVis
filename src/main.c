0a27a79dff2046bb5e948a0343d28f311492790f
TerrenceCao1 <terrencezcao@gmail.com>
2025-08-13 22:39:51 -0700

    added LED matrix wiring

--- src/main.c
+++ src/main.c
@@ -17,7 +17,20 @@
     L/R: GND
 
     LED ARRAY WIRING:
-    
+    RED0: GPIO 4
+    RED1: GPIO 12
+    GREEN0: GPIO 5
+    GREEN1: GPIO 13
+    BLUE0: GPIO 14
+    BLUE1: GPIO 16
+    A: GPIO 17
+    B: GPIO 18
+    C: GPIO 19
+    D: GPIO 21
+    CLK: GPIO 22
+    LATCH: GPIO 23
+    OE: GPIO 27
+    GND: GND
 */
 
 void app_main() {
