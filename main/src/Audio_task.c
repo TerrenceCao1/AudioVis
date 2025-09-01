12b909ea44717e683980b7eb4e3981303ab9111c
TerrenceCao1 <terrencezcao@gmail.com>
2025-09-01 00:22:16 -0700

    changed semaphore to queue, fixed synchronization

--- main/src/Audio_task.c
+++ main/src/Audio_task.c
@@ -1,7 +1,18 @@
 #include "Audio_task.h"
 #include "driver/i2s.h"
+#include "portmacro.h"
 #include "string.h"
 #include "freertos/queue.h"
+#include "math.h"
+#include "fft.h"
+
+QueueHandle_t bufferQueue;
+
+float bufferA[BUFFER_SIZE];
+float bufferB[BUFFER_SIZE];
+
+float *samplingBuffer = bufferA;
+float *fftBuffer = bufferB;
 
 void I2S_Init(i2s_config_t *mainConfig, i2s_pin_config_t *pinConfig)
 {
@@ -29,60 +40,73 @@ void I2S_Init(i2s_config_t *mainConfig, i2s_pin_config_t *pinConfig)
     i2s_set_pin(I2S_PORT, pinConfig);
 }
 
-//initing Semaphores
-SemaphoreHandle_t xBufferReadySem; //semaphore for when buffer is ready
-
-//buffer initialization
-float bufferA[BUFFER_SIZE];
-float bufferB[BUFFER_SIZE];
-
-float *samplingBuffer = bufferA;
-float *fftBuffer = bufferB;
-
 void sampleAudioData(void * pvParameter)
 {
     size_t bytes_read;
 
     while (1)
     {
-        printf("started sampling \n");
+		printf("sampling\n");
+		//error check + populate buffer:
+		esp_err_t ret = i2s_read(I2S_NUM_0, samplingBuffer, sizeof(uint32_t) * BUFFER_SIZE, &bytes_read, portMAX_DELAY);
 
-        //error check + populate buffer:
-        esp_err_t ret = i2s_read(I2S_NUM_0, samplingBuffer, sizeof(uint32_t) * BUFFER_SIZE, &bytes_read, portMAX_DELAY);
+		if (ret != ESP_OK || bytes_read != sizeof(float) * BUFFER_SIZE)
+		{
+			printf("I2S read Error: %d, bytes: %d\n", ret, bytes_read);
+			vTaskDelay(pdMS_TO_TICKS(10));
+			continue;
+		}
 
-        if (ret != ESP_OK || bytes_read != sizeof(float) * BUFFER_SIZE)
-        {
-            printf("I2S read Error: %d, bytes: %d\n", ret, bytes_read);
-            vTaskDelay(pdMS_TO_TICKS(10));
-            continue;
-        }
-
-        //copy the sampling buffer over to the fftBuffer through temp
-        float *temp = fftBuffer;
-        fftBuffer = samplingBuffer;
-        samplingBuffer = temp;
-
-        //let FFT function know that the buffer is ready
-        xSemaphoreGive(xBufferReadySem);
-        printf("END OF SAMPLING \n");
+		//copy the sampling buffer over to the fftBuffer through temp
+		float *temp = fftBuffer;
+		fftBuffer = samplingBuffer;
+		samplingBuffer = temp;
+		
+		xQueueSend(bufferQueue, &fftBuffer, portMAX_DELAY);
+		printf("sampling done\n");
     }
 }
 
 void xFFT(void* pvParameters)
 {
-    //init fft_config
     fft_config_t *fft_config = fft_init(512, FFT_REAL, FFT_FORWARD, NULL, NULL);
-    
-    while (1)
-    {
-        if(xSemaphoreTake(xBufferReadySem, portMAX_DELAY))
-        {
-            printf("start of FFT\n");
-            memcpy(fft_config->input, fftBuffer, sizeof(float) * BUFFER_SIZE);
-            fft_execute(fft_config);
-            printf("end of FFT\n");
-        }
-        taskYIELD();        
-    }
+
+	int bandBins[FFT_BANDS+1]; //number of bands + 1 for edges
+	for(int i = 0; i < FFT_BANDS+1; i++)
+	{
+		//exponentially sized bins since human hearing is logarithmic
+		float f = pow(256, (float)i/FFT_BANDS);
+		bandBins[i] = (int)f;
+		
+		//make sure each frequency bin is actually different (low end is all the same)
+		if(i > 0 && (bandBins[i] <=bandBins[i-1]))
+		{
+			bandBins[i] = bandBins[i-1] + 1;
+		}
+	}
+
+	int bandAmps[FFT_BANDS];
+	
+		while (1)
+		{
+			if (xQueueReceive(bufferQueue, &fftBuffer, portMAX_DELAY))
+			{
+				printf("start of FFT\n");
+				memcpy(fft_config->input, fftBuffer, sizeof(float) * BUFFER_SIZE);
+				fft_execute(fft_config);
+				
+				//for each bin, we add up all the fft real values between those bounds.
+				for (int i = 0; i < FFT_BANDS; i++)
+				{
+					for(int j = bandBins[i]; j < bandBins[i+1]; j++)
+					{
+						bandAmps[i] += (int)fft_config->output[2*j];
+					}
+					bandAmps[i] /= bandBins[i+1] - bandBins[i];
+					printf("Bin %i Amp: %i\n", i, bandAmps[i]);
+				}
+			}
+		}
+    taskYIELD();        
     fft_destroy(fft_config);
 }
