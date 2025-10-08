dd3b5acac77aec94a631cffb72912b7c783496eb
TerrenceCao1 <terrencezcao@gmail.com>
2025-10-07 23:55:48 -0700

    added LED task to main

--- test/testLEDLib/main/main.c
+++ test/testLEDLib/main/main.c
@@ -1,21 +1,38 @@
-#include "esp_rom_sys.h"
-#include "matrix_wrapper.h"
-#include "stdio.h"
+#include <matrix_wrapper.h>
+#include "LED_task.h"
+#include "Audio_task.h"
 
-int levels[WIDTH];
+/*
+    INMP441 PINOUT/WIRING:
+    VDD: 3v2
+    GND: GND
+    SD: GPIO 33
+    SCK: GPIO 32
+    WS: GPIO 25
+    L/R: GND
 
-void app_main(void)
-{
-	matrix_init(WIDTH, HEIGHT);	
-	int i = 0;
-	while(i < WIDTH)
-	{
-		levels[i] = i;
-		i++;
-	}
+    LED ARRAY WIRING:
+    RED0: GPIO 4
+    RED1: GPIO 12
+    GREEN0: GPIO 5
+    GREEN1: GPIO 13
+    BLUE0: GPIO 14
+    BLUE1: GPIO 16
+    A: GPIO 17
+    B: GPIO 18
+    C: GPIO 19
+    D: GPIO 21
+    CLK: GPIO 22
+    LATCH: GPIO 23
+    OE: GPIO 27
+    GND: GND
+*/
 
-	while(1)
-	{
-		matrix_draw_audio_levels(levels);
-	}
+void app_main() {
+	bufferQueue = xQueueCreate(1, sizeof(float*));
+	fftToLEDQueue = xQueueCreate(1, sizeof(float*));
+
+    xTaskCreatePinnedToCore(sampleAudioData, "SamplingI2S", 8192, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(xFFT, "FFT", 8192, NULL, 5, NULL, 0);
+	xTaskCreatePinnedToCore(xDrawLEDLevels, "DrawLEDLevels", 8192, NULL, 5, NULL, 1);
 }
