8144b44f1fd6b4683bed173afa624bef74b41c5a
TerrenceCao1 <terrencezcao@gmail.com>
2025-10-07 23:53:54 -0700

    created queues for LEDMatrix task

---
+++ test/testLEDLib/main/Audio_task.c
@@ -0,0 +1,139 @@
+#include "Audio_task.h"
+#include "driver/i2s_common.h"
+#include "driver/i2s_types.h"
+#include "esp_err.h"
+#include "freertos/projdefs.h"
+#include "hal/i2s_types.h"
+#include "portmacro.h"
+#include "string.h"
+#include "freertos/queue.h"
+#include "driver/i2s_std.h"
+
+QueueHandle_t bufferQueue;
+QueueHandle_t fftToLEDQueue;
+
+
+int32_t samplingBuffer[BUFFER_SIZE];
+float fftBuffer[BUFFER_SIZE];
+
+i2s_chan_handle_t rx_handle;
+i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_PORT, I2S_ROLE_MASTER);
+
+void i2s_init(void)
+{
+    ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, NULL, &rx_handle));
+
+    i2s_std_config_t std_cfg = {
+        .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG(44100),
+        .slot_cfg = {
+            .data_bit_width = I2S_DATA_BIT_WIDTH_32BIT,
+            .slot_bit_width = I2S_SLOT_BIT_WIDTH_32BIT,
+            .slot_mode      = I2S_SLOT_MODE_MONO,
+			.slot_mask      = I2S_STD_SLOT_LEFT,
+            .ws_width       = I2S_DATA_BIT_WIDTH_32BIT,
+            .ws_pol         = false,
+            .bit_shift      = true,
+            .msb_right      = false,
+        },
+        .gpio_cfg = {
+            .mclk       = I2S_GPIO_UNUSED,
+            .bclk       = I2S_SCK,
+            .ws         = I2S_WS,
+            .dout       = I2S_GPIO_UNUSED,
+            .din        = I2S_SD,
+            .invert_flags = {
+                .mclk_inv = false,
+                .bclk_inv = false,
+                .ws_inv   = false,
+            },
+        },
+    };
+
+    ESP_ERROR_CHECK(i2s_channel_init_std_mode(rx_handle, &std_cfg));
+
+    ESP_ERROR_CHECK(i2s_channel_enable(rx_handle));
+}
+
+
+
+void sampleAudioData(void * pvParameter)
+{
+    size_t bytes_read;
+	i2s_init();
+
+    while (1)
+    {
+		esp_err_t ret = i2s_channel_read(rx_handle, samplingBuffer, sizeof(samplingBuffer), &bytes_read, portMAX_DELAY);
+
+		if (ret != ESP_OK)
+		{
+			printf("I2S read Error: %d, bytes: %u\n", ret, bytes_read);
+			vTaskDelay(pdMS_TO_TICKS(10));
+			while(1);
+		}
+
+		//INMP441 gives us 24 bit i2s, so we need to bitshift and typecast to float
+		for(int i = 0; i < BUFFER_SIZE; i++)
+		{
+			int32_t s24 = samplingBuffer[i] >> 8;
+			fftBuffer[i] = (float)s24 * 100/ 8388608.0f; //2^32 to normalize the float
+		}
+		
+		if(xQueueSend(bufferQueue, &fftBuffer, portMAX_DELAY))
+		{
+			printf("sampling done\n");
+		}
+
+		vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+#define FFT_BANDS 32
+#define MIN_BAND_VALUE 2
+#define MAX_BAND_VALUE 128
+
+void xFFT(void* pvParameters)
+{
+    fft_config_t *fft_config = fft_init(BUFFER_SIZE, FFT_REAL, FFT_FORWARD, NULL, NULL);
+
+	int bandBins[FFT_BANDS+1]; //number of bands + 1 for edges
+	bandBins[0] = 2;
+	for(int i = 1; i < FFT_BANDS+1; i++)
+	{
+		double ratio = (double)i/FFT_BANDS;
+		double value = MIN_BAND_VALUE * pow((double)MAX_BAND_VALUE/MIN_BAND_VALUE, ratio);
+
+		bandBins[i] = (int)round(value);
+
+		if(bandBins[i] <= bandBins[i-1])
+		{
+			bandBins[i] = bandBins[i-1] + 1;
+		}
+	}
+
+	float bandAmps[FFT_BANDS];
+	while (1)
+	{
+		if (xQueueReceive(bufferQueue, &fftBuffer, portMAX_DELAY))
+		{
+			printf("start of FFT\n");
+			memcpy(fft_config->input, &fftBuffer, BUFFER_SIZE * sizeof(float));
+			fft_execute(fft_config);
+			
+			for (int i = 0; i < FFT_BANDS; i++)
+			{
+				for(int j = bandBins[i]; j < bandBins[i+1]; j++)
+				{
+					bandAmps[i] += sqrtf(pow(fft_config->output[2*j], 2) + pow(fft_config->output[2*j + 1], 2));
+				}
+				printf("%i, %f\n", i, bandAmps[i]);
+			}
+		}
+		memset(bandAmps, 0, sizeof(bandAmps));
+		xQueueSend(fftToLEDQueue, &fftBuffer, portMa)
+
+		vTaskDelay(pdMS_TO_TICKS(10));
+	}
+    taskYIELD();        
+    fft_destroy(fft_config);
+}
---
+++ test/testLEDLib/main/Audio_task.h
@@ -0,0 +1,52 @@
+#include "driver/i2s_common.h"
+#include "driver/i2s_std.h"
+#include "driver/i2s_types.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include <math.h>
+#include <fft.h>
+
+#ifndef AUDIO_TASK_h_
+#define AUDIO_TASK_h_
+
+#define I2S_SCK GPIO_NUM_32
+#define I2S_WS GPIO_NUM_25
+#define I2S_SD GPIO_NUM_33
+#define I2S_PORT I2S_NUM_0
+#define BUFFER_SIZE 512
+#define FFT_BANDS 32
+
+extern QueueHandle_t bufferQueue;
+extern QueueHandle_t fftToLEDQueue;
+extern int32_t samplingBuffer[BUFFER_SIZE];
+extern float fftBuffer[BUFFER_SIZE];
+
+/*
+    functions needed:
+        I2S_Init
+        Sample_I2S_Audio
+        FFT_Audio_Sample
+*/
+
+/*
+    function: I2S_Init -> initialize i2s_config and i2s_pin_config and install i2s driver
+    input: void
+    output: void
+*/
+void i2s_init(void);
+
+/*
+    function: sampleAudioData -> samples I2S audio data from IMNP441 microphone
+    input: buffer array to be filled with data
+    output: buffer gets filled with audio data
+*/
+void sampleAudioData(void *pvParameter);
+
+/*
+    function: fastFourierTransform -> takes buffer array and performs a fast fourier transform
+    input: pointer to buffer containing audio data
+    output: void
+*/
+void xFFT(void *pvParameter);
+
+#endif //AUDIO_TASK_h_
