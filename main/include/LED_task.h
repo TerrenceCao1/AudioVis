7699262c93bb9cbc904d0e6012bb8e24c87889e2
Terrence Cao <terrencezcao@gmail.com>
2025-11-03 17:08:41 -0800

    added color changing button!

--- main/include/LED_task.h
+++ main/include/LED_task.h
@@ -1,3 +1,6 @@
+#ifndef LED_TASK_h_
+#define LED_TASK_h_
+
 #include "freertos/idf_additions.h"
 #include <math.h>
 #include <driver/i2s.h>
@@ -6,11 +9,13 @@
 #include "matrix_wrapper.h"
 #include "freertos/queue.h"
 #include "portmacro.h"
+#include "driver/gpio.h"
 
-#ifndef LED_TASK_h_
-#define LED_TASK_h_
+
+#define BUTTON_PIN 35
 
 extern QueueHandle_t fftToLEDQueue;
+extern QueueHandle_t interputQueue;
 
 /*
  * @brief	Takes data from FFT function and displays onto the LED Matrix
@@ -20,5 +25,27 @@ extern QueueHandle_t fftToLEDQueue;
  * @return	void
 */
 void xDrawLEDLevels(void *pvParameter);
+
+
+/*
+ * @brief	Initiate GPIOS for interrupt
+ *
+ *			Sets BUTTON_PIN as an input that can take an interrupt
+ *
+ * @return	void
+ *
+ * */
+void setInterruptGPIOs(void);
+
+/*
+ * @brief	Takes button press interrupt and updates counter such that LED Color changes
+ *
+ *			This function takes from interputQueue and updates a counter in order to cycle through a color array
+ *
+ *	@return void
+ * */
+void vColorChange(void* params);
+
+
 
 #endif //LED_TASK_h_
--- main/src/LED_task.c
+++ main/src/LED_task.c
@@ -1,10 +1,17 @@
 #include "LED_task.h"
+#include "driver/gpio.h"
+#include "esp_rom_gpio.h"
 #include "freertos/idf_additions.h"
 #include "freertos/projdefs.h"
+#include "hal/gpio_types.h"
 #include "matrix_wrapper.h"
 #include "portmacro.h"
 
 // TODO: Color changing Button!
+int buttonPressCount = 0;
+static const uint16_t colors[] = {0xFFFF, 0xF000, 0x3EBF, 0xEC9D, 0x07E4, 0x20FE};
+#define COLOR_COUNT (sizeof(colors) / sizeof(colors[0]))
+
 void xDrawLEDLevels(void *pvParameter)
 {
 	matrix_init(WIDTH, HEIGHT);
@@ -13,7 +20,7 @@ void xDrawLEDLevels(void *pvParameter)
 		if(xSemaphoreTake(LEDBufferMutex, portMAX_DELAY))
 		{
 			matrix_clear();
-			matrix_draw_audio_levels(LED_Buffer, 0xFFFF);
+			matrix_draw_audio_levels(LED_Buffer, colors[buttonPressCount % COLOR_COUNT]);
 
 			xSemaphoreGive(LEDBufferMutex);
 		}
@@ -21,19 +28,47 @@ void xDrawLEDLevels(void *pvParameter)
 	}
 }
 
-void xPrintLEDBuffer(void *pvParameter)
+//Button Interrupt to change colors
+QueueHandle_t interputQueue;
+
+
+static uint32_t lastInterruptTime = 0;
+static void IRAM_ATTR gpio_interrupt_handler(void *args)
 {
-	float floatLEDBuffer[WIDTH/2];
-	matrix_init(WIDTH, HEIGHT);
+	int pinNumber = (int)args;
+	uint32_t currentTime = xTaskGetTickCountFromISR();
+
+	if(currentTime - lastInterruptTime > pdMS_TO_TICKS(200))
+	{
+		xQueueSendFromISR(interputQueue, &pinNumber, NULL);
+		lastInterruptTime = currentTime;
+	}
+}
+
+void setInterruptGPIOs(void)
+{
+	esp_rom_gpio_pad_select_gpio(BUTTON_PIN);
+	gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
+	gpio_pulldown_en(BUTTON_PIN);
+	gpio_pullup_dis(BUTTON_PIN);
+	gpio_set_intr_type(BUTTON_PIN, GPIO_INTR_POSEDGE);
+
+	gpio_install_isr_service(0);
+	gpio_isr_handler_add(BUTTON_PIN, gpio_interrupt_handler, (void*)BUTTON_PIN);
+}
+
+void vColorChange(void* params)
+{
+	int pinNumber;
 	while(1)
 	{
-		if(xQueueReceive(fftToLEDQueue, &floatLEDBuffer, portMAX_DELAY)) 
+		if(xQueueReceive(interputQueue, &pinNumber, portMAX_DELAY))
 		{
-			for(int i = 0; i < WIDTH/2; i++)
+			vTaskDelay(pdMS_TO_TICKS(50));
+			if(gpio_get_level(BUTTON_PIN))
 			{
-				printf("floatLEDBuffer[%i]: %f\n", i, floatLEDBuffer[i]);
+				buttonPressCount++;			
 			}
-			vTaskDelay(pdTICKS_TO_MS(10));
 		}
 	}
 }
